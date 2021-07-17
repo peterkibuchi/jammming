8a8ae0ef0810bcc45c47dbcce36816e9477dbdc1
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2021-07-17 08:14:40 +0300

    Edit App Description

--- src/components/AppDescription/AppDescription.js
+++ src/components/AppDescription/AppDescription.js
@@ -6,11 +6,12 @@ export class AppDescription extends Component {
         return (
             <div className="Description">
                 <p>
-                    Welcome. Jammming is a website that allows you to search the Spotify library, 
-                    create a custom playlist, name it, and save it to your Spotify account.
+                    Welcome. Jammming is a web application that allows you to search the Spotify 
+                    library, create a custom playlist, name it, and save it to your Spotify account. 
+                    Playlists will be set to private by default.
                 </p>
                 <p>
-                    To function, the app will require you to log in to your Spotify account, after which Jammming 
+                    To function, Jammming will require you to log in to your Spotify account, after which it 
                     will request permission to act on your behalf. You can revoke this permission at any time at 
                     <a href="https://www.spotify.com/account/apps/" target="_blank" rel="noopener noreferrer"> spotify.com/account</a>.
                 </p>
